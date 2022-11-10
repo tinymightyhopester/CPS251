8cdcc6fb6477ef5cd6d21ec72cc95a3301e74347
Hope <creatinghopeagain@gmail.com>
2022-11-09 21:12:18 -0500

    cardProject

---
+++ Assignments/CardProject/app/src/main/java/com/example/cardproject/RecyclerAdapter.kt
@@ -0,0 +1,66 @@
+package com.example.cardproject
+
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.TextView
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.snackbar.Snackbar
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cardproject.R
+
+class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var itemImage: ImageView
+        var itemTitle: TextView
+        var itemDetail: TextView
+        init {
+            itemImage = itemView.findViewById(R.id.itemImage)
+            itemTitle = itemView.findViewById(R.id.itemTitle)
+            itemDetail = itemView.findViewById(R.id.itemDetail)
+
+            itemView.setOnClickListener { v: View ->
+                var position: Int = getAdapterPosition()
+                Snackbar.make(v, "Click detected on item $position",
+                    Snackbar.LENGTH_LONG).setAction("Action", null).show()
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
+        val v = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.card_layout, viewGroup, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
+        nums.shuffle()
+        viewHolder.itemTitle.text =  titles[nums[i]]
+
+        viewHolder.itemDetail.text = details[nums[i]]
+        viewHolder.itemImage.setImageResource(images[nums[i]])
+    }
+
+    override fun getItemCount(): Int {
+        return titles.size
+    }
+    private val nums = intArrayOf(0,1,2,3,4,5,6,7,8)
+
+    private val titles = arrayOf("Chapter One",
+        "Chapter Two", "Chapter Three", "Chapter Four",
+        "Chapter Five", "Chapter Six", "Chapter Seven",
+        "Chapter Eight")
+
+    private val details = arrayOf("Item one details", "Item two details",
+        "Item three details", "Item four details",
+        "Item five details", "Item six details",
+        "Item seven details", "Item eight details")
+
+    private val images = intArrayOf(R.drawable.android_image_1,
+        R.drawable.android_image_2, R.drawable.android_image_3,
+        R.drawable.android_image_4, R.drawable.android_image_5,
+        R.drawable.android_image_6, R.drawable.android_image_7,
+        R.drawable.android_image_8)
+
+}
